5a4c195f7a81e0a36da9d0b6bfb6b6b26ec64147
Fábio França <fabiotibete@gmail.com>
2020-08-03 10:04:17 -0300

    create project

---
+++ StarWars/StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Fábio França on 03/08/20.
+//  Copyright © 2020 music.com.chama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
